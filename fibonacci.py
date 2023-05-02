d787b7d850f0b614bbaffc8999b438f3810d037e
bgebelek <113651363+bgebelek@users.noreply.github.com>
2023-05-02 12:36:23 -0400

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+# Practice Python - Exercise 13 - Fibonacci
+
+
+def fibonnaci():
+    num = int(input("Number: "))
+    if num < 0:
+        return "Error"
+    elif num == 0:
+        return []
+    else:
+        result = [0, 1]
+        for _ in range(num - 1):
+            result.append(result[-2] + result[-1])
+        return result[1:]
+
+
+print(fibonnaci())
