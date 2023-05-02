89937733ce7f4cceb50e6d3523bb7db367efc674
bgebelek <113651363+bgebelek@users.noreply.github.com>
2023-05-02 12:51:29 -0400

    Create  return_the_time_saved_by_speeding.py

---
+++ return_the_time_saved_by_speeding.py
@@ -0,0 +1,8 @@
+# Edabit - Return the Time Saved by Speeding
+
+
+def time_saved(speed_limit, average_speed, average_speed_distance):
+    return round(((average_speed_distance * 60) / speed_limit) - ((average_speed_distance * 60) / average_speed), 1)
+
+
+print(time_saved(80, 100, 10))
