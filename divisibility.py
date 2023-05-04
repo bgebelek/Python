cbc510e9605af9a36e7a035d88d904331e26f9b0
bgebelek <113651363+bgebelek@users.noreply.github.com>
2023-05-03 20:13:43 -0400

    Create divisibility.py

---
+++ divisibility.py
@@ -0,0 +1,11 @@
+##HackerEarth - Divisibility
+
+
+def formed_number_divisible_by_10(numbers):
+    for number in numbers:
+        if str(number)[-1] != 0:
+            return "No"
+    return "Yes"
+
+
+print(formed_number_divisible_by_10([85, 25, 65, 21, 84]))
